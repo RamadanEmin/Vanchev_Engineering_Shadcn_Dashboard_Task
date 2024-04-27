ed1153f18a05f2aaa61868e299aa7c28d34ab87a
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-04-27 09:18:48 +0300

    Integration into dashboard navigation and team switching functionalities

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import { Metadata } from 'next';
+import Image from 'next/image';
 
-const Dashboard = () => {
+import { MainNav } from '@/components/main-nav';
+import TeamSwitcher from '@/components/team-switcher';
+
+export const metadata: Metadata = {
+    title: 'Dashboard',
+    description: 'Example dashboard app built using the components.',
+};
+
+export default function DashboardPage() {
     return (
-        <div>Dashboard</div>
+        <>
+            <div className="md:hidden">
+                <Image
+                    src="/examples/dashboard-light.png"
+                    width={1280}
+                    height={866}
+                    alt="Dashboard"
+                    className="block dark:hidden"
+                />
+                <Image
+                    src="/examples/dashboard-dark.png"
+                    width={1280}
+                    height={866}
+                    alt="Dashboard"
+                    className="hidden dark:block"
+                />
+            </div>
+            <div className="hidden flex-col md:flex">
+                <div className="border-b">
+                    <div className="flex h-16 items-center px-4">
+                        <TeamSwitcher />
+                        <MainNav className="mx-6" />
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
-
-export default Dashboard;
